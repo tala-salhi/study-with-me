76ab5c95feec8075513ae2668ab3420e819d82be
tala-salhi <talasalhi8@gmail.com>
2025-05-21 22:38:08 +0400

    Update StudyWithMeApp.jsx

--- StudyWithMeApp.jsx
+++ StudyWithMeApp.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
-import { Card, CardContent } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "./components/ui/card";
+import { Button } from "./components/ui/button";
 import { motion } from "framer-motion";
 import { Upload } from "lucide-react";
 import { initializeApp } from "firebase/app";
